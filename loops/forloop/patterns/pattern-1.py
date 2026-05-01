ff08b5faebc51771e123f7bb4d60088272af6a82
arun <allamarunkumar@gmail.com>
2026-05-01 17:35:34 +0530

    latha updated

--- loops/forloop/patterns/pattern-1.py
+++ loops/forloop/patterns/pattern-1.py
@@ -5,17 +5,17 @@ num = 5
 1   2   3
 1   2   3   4
 1   2   3   4   5
-rows-5
-ro-1   1 col    
-ro-2   1 col 2col
-ro-3   1 col 2col 3col
-ro-4   1 
+row - 1    1
+row - 2    1    2
+row - 3    1    2   3
+row - 4    1    2   3   4
+row - 5    1    2   3   4   5
+
+
+
 '''
-num = int(input('Enter a number : '))#5
-k = 1
+num = int(input('Enter a number : '))
 for i in range(1,num+1):
     for j in range(1,i+1):
-        print(k,end="\t")
-        k = k+1
+        print(f'{i}',end="  ")
     print()
-
--- loops/forloop/patterns/pattern-2.py
+++ loops/forloop/patterns/pattern-2.py
@@ -11,15 +11,16 @@ num = 5
 1
 '''
 num = int(input('Enter a number : '))#num = 5
-
+k = 65
 for i in range(1,num+1):
     for j in range(1,i+1):
-        print(f'{j}',end="\t")
+        print(chr(k),end="  ") # chr is a function which can convert ASCII into alphabet A - 65
+        k = k+1
     print()
-    
-for i in range(num-1,0,-1):
-    for j in range(1,i+1):
-        print(f'{j}',end="\t")
+for i in range(num,0,-1):
+    for j in range(1,i):
+        print(chr(k),end="  ")
+        k = k+1
     print()
         
     
